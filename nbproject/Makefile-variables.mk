609521303f7c4790576a097f41f50aabc7753a0f
tsobieroy <tsobieroy@linux>
2017-08-07 15:35:58 +0200

    final commit

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=GNU-None
-CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-None
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
 CND_ARTIFACT_NAME_Debug=raspberrypi-ph-calibration
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-None/raspberrypi-ph-calibration
-CND_PACKAGE_DIR_Debug=dist/Debug/GNU-None/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/raspberrypi-ph-calibration
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
 CND_PACKAGE_NAME_Debug=raspberrypi-ph-calibration.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-None/package/raspberrypi-ph-calibration.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/raspberrypi-ph-calibration.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
